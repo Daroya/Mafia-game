0bf26c4e5978d53336d2623c097a55dc06f72eaa
daroya <gavriluka628@gmail.com>
2025-12-21 20:19:44 +0200

    Logic day and add sheriff

--- game/logic.py
+++ game/logic.py
@@ -21,32 +21,33 @@ def create_players(names):
     return players
 
 def night(players):
-    night_has_comeprint = ("Настала ніч")
+    print("Настала ніч!")
+
 
     alive_players = []
 
     # 1. збираємо живих
-    for p in players:
-        if p.alive == True:
-            alive_players.append(p)
+    for pn in players:
+        if pn.alive == True:
+            alive_players.append(pn)
 
-    # 2. шукаємо мафію
+    #Логіка мафії
     mafia_players = []
-    for p in alive_players:
-        if isinstance(p.role, Mafia):
-            mafia_players.append(p)
+    for pn in alive_players:
+        if isinstance(pn.role, Mafia):
+            mafia_players.append(pn)
 
     if len(mafia_players) == 0:
         return
 
-    # 3. мафія вибирає жертву
+    # мафія вибирає жертву
     victim = random.choice(alive_players)
 
-    # 4. шукаємо лікаря
+    #Логіка лікаря
     doctors = []
-    for p in alive_players:
-        if isinstance(p.role, Doctor):
-            doctors.append(p)
+    for pn in alive_players:
+        if isinstance(pn.role, Doctor):
+            doctors.append(pn)
 
     healed = None
     if len(doctors) > 0:
@@ -54,4 +55,33 @@ def night(players):
 
     if victim != healed:
         victim.kill()
+        print(f"Вночі загинув: {victim.name}")
+    
+    #Логіка Ширіфа
+    sheriff = []
+    for pn in alive_players:
+        if isinstance(pn.role, Sheriff):
+            sheriff.append(pn)
+    #Рандом вибір кого вбити ширіфа
+    sheriff_victim = random.choice(alive_players)
+    if victim == sheriff:
+        sheriff.kill()
+    else:
+        sheriff_victim.kill()
+    print(f'Сьогодні Шериф убив {sheriff_victim.name}')
 
+#логика дня
+def day(players):
+    print("Настав день!")
+
+    alive_players = []
+#Провірка чи живий гравець і чи може він голосуати
+    for pd in players:
+        if pd.alive == True:
+            alive_players.append(pd)
+#Рандом голосованіє
+    voted_player = random.choice(alive_players)
+#Вибити людей за яких голосували
+    if voted_player.alive == True:
+        voted_player.kill()
+    print(f"Вигнали гравця: {voted_player.name}")
--- main.py
+++ main.py
@@ -1,8 +1,48 @@
-from game.logic import create_players, night
+from game.state import Player
+from game.roles import Mafia, Doctor, Civilian, Sheriff
+from game.logic import night, day
 
-names = ["Ярік", "Остап", "Павло", "Бот1", "Бот2"]
+# створюємо гравців це дньои зробив щас буду робити всьо для шерифа і ось боти чекай
+players = [
+    Player("Ярослав", Mafia()),
+    Player("Остап", Doctor()),
+    Player("Павло", Civilian()),
+    Player("Бот1", Civilian()),
+    Player("Бот2", Sheriff()) 
+]
 
-players = create_players(names)
+#Тест гри в консолі на ботах
+def check_win(players):
+    mafia_alive = 0
+    civilians_alive = 0
 
-for p in players:
-    print(p)
+    for p in players:
+        if p.alive == True:
+            if isinstance(p.role, Mafia):
+                mafia_alive += 1
+            else:
+                civilians_alive += 1
+
+    if mafia_alive == 0:
+        print("Мирні перемогли!")
+        return True
+
+    if mafia_alive >= civilians_alive:
+        print("Мафія перемогла!")
+        return True
+
+    return False
+
+
+print("Гра почалась!")
+
+while True:
+    night(players)
+
+    if check_win(players):
+        break
+
+    day(players)
+
+    if check_win(players):
+        break
